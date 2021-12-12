14d166237d0925489e28148d77236cc8aebb5013
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2021-12-12 16:16:43 +0900

    update ch4.2.0 R

--- Code_R/ch4_2_0.R
+++ Code_R/ch4_2_0.R
@@ -87,3 +87,103 @@ ggplot(logit_df, aes(x = sigma, y = a)) +
        x = expression(sigma))
 
 
+
+# 導関数と接線の関係 ---------------------------------------------------------------
+
+# 追加パッケージ
+library(gganimate)
+
+
+# 関数を指定
+f <- function(x) {
+  y <- 1 / (1 + exp(-x)) # Sigmoid
+  #y <- (exp(x) - exp(-x)) / (exp(x) + exp(-x)) # tanh
+  return(y)
+}
+
+# 導関数を指定
+df <- function(y) { 
+  dy <- y * (1 - y) # Sigmoid
+  #dy <- 1 - y^2 # tanh
+  return(dy)
+}
+
+
+# x軸の値を作成
+x_vals <- seq(-10, 10, by = 0.1)
+length(x_vals) # フレーム数  
+
+# Sigmoid関数を計算
+sigmoid_df <- tidyr::tibble(
+  x = x_vals, 
+  y = f(x) # 出力
+)
+
+# xの値ごとにグラフを計算
+anime_diff_df <- tidyr::tibble()
+anime_tangentline_df <- tidyr::tibble()
+anime_tangentpoint_df <- tidyr::tibble()
+for(i in 1:length(x_vals)) {
+  # 接点のx軸の値を取得
+  x_val <- x_vals[i]
+  
+  # 接点のy軸の値を計算
+  y_val <- f(x_val)
+  
+  # 接線の傾きを計算
+  dy_val <- df(y_val)
+  
+  # 接線の切片を計算
+  b_val <- y_val- dy_val * x_val
+  
+  # フレーム切替用のラベルを作成
+  label_txt <- paste(
+    "(x, y)=(", round(x_val, 2), ", ", round(y_val, 2), "), dy=", round(dy_val, 3)
+  )
+  
+  # 導関数を計算
+  tmp_diff_df <- tidyr::tibble(
+    x = x_vals[1:i], 
+    y = f(x), # 出力
+    dy = df(y), # 微分
+    label = as.factor(label_txt)
+  )
+  anime_diff_df <- rbind(anime_diff_df, tmp_diff_df)
+  
+  # 接線を計算
+  tmp_tangentline_df <- tidyr::tibble(
+    x = x_vals, 
+    y = dy_val * x + b_val, 
+    label = as.factor(label_txt)
+  )
+  anime_tangentline_df <- rbind(anime_tangentline_df, tmp_tangentline_df)
+  
+  # 接点を格納
+  tmp_tangentpoint_df <- tidyr::tibble(
+    x = x_val, 
+    y = y_val, 
+    label = as.factor(label_txt)
+  )
+  anime_tangentpoint_df <- rbind(anime_tangentpoint_df, tmp_tangentpoint_df)
+  
+  # 途中経過を表示
+  message("\r", rep(" ", 20), appendLF = FALSE)
+  message("\r", i, " (", round(i / length(x_vals) * 100, 2), "%)", appendLF = FALSE)
+}
+
+# 作図
+anime_graph <- ggplot() + 
+  geom_line(data = sigmoid_df, aes(x = x, y = y, group = 1), color = "blue") + # 対象の関数
+  geom_line(data = anime_diff_df, aes(x = x, y = dy, group = 1), color = "orange") + # 導関数
+  geom_line(data = anime_tangentline_df, aes(x = x, y = y, group = 1), color = "turquoise4") + # 接線
+  geom_point(data = anime_tangentpoint_df, aes(x = x, y = y), color = "chocolate4", size = 2) + # 接点
+  geom_vline(data = anime_tangentpoint_df, aes(xintercept = x), color = "chocolate4", linetype = "dotted") + # 接線の垂線
+  gganimate::transition_manual(label) + # フレーム
+  ylim(c(-0.5, 1.5)) + 
+  labs(title = "Sigmoid Function", 
+       subtitle = "{current_frame}")
+
+# gif画像として出力
+gganimate::animate(anime_graph, nframes = length(x_vals), fps = 10)
+
+
